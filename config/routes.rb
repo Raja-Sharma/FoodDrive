aa9f400cb891acdab333933d0ca689c1329fb2cb
Raja Sharma <rajasharma55@gmail.com>
2017-08-06 02:27:47 -0400

    Add registration and user authorization

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create]
   resources :foods
 
+  get 'login', to: 'sessions#new'
+  post 'sessions', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   root to: 'foods#index'
 
 end
