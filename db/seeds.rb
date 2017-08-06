7f5fcb56526c3a4bac96a3f86b975f9bfb847dd7
Raja Sharma <rajasharma55@gmail.com>
2017-08-05 23:39:57 -0400

    Add donor relationship between food and user models. Add seed data. Add seed file to git ignore.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Food.delete_all
+ClaimedFood.delete_all
+
+raja = User.new(
+  username: 'Raja',
+  email: 'me@me.com',
+  password: 'password'
+)
+
+raja.save
+
+joe = User.new(
+  username: 'Joe',
+  email: 'joe@joe.com',
+  password: 'password'
+)
+
+joe.save
+
+beans = Food.new(
+  name: 'beans',
+  category: 'canned',
+  donor: raja
+)
+
+beans.save
+
+rice = Food.new(
+  name: 'rice',
+  category: 'canned',
+  donor: raja
+)
+
+rice.save
+
+ClaimedFood.create(
+  food: rice,
+  user: joe
+)
+
+ClaimedFood.create(
+  food: beans,
+  user: joe
+)
